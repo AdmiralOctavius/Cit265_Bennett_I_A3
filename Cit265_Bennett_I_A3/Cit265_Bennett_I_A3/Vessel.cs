5c271aeb2a5842ea9760d8dcb7898adf9408e1ce
AdmiralOct <buildgamessnotbombs@gmail.com>
2018-10-03 00:57:15 -0400

    Put Project on Local Machine

---
+++ Cit265_Bennett_I_A3/Cit265_Bennett_I_A3/Vessel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIT263_Bennett_I_A3
+{
+    public class Vessel
+    {
+        public string name;
+        public float speed;
+
+        public virtual void Move()
+        {
+            Console.WriteLine($"Moving at {speed}.");
+        }
+    }
+
+    public class Ship : Vessel
+    {
+        public int fuel;
+        public int maxFuel;
+
+        public override void Move()
+        {
+            if(fuel > 0)
+            {
+                Console.WriteLine($"Moving at {speed}.");
+                Console.WriteLine("Removing 1 fuel");
+            }
+        }
+    }
+
+    public class Boat : Vessel
+    {
+        public int oars;
+
+        public override void Move()
+        {
+            Console.WriteLine($"Moving at {speed * oars}.");
+        }
+    }
+    //Interface is a lightweight structure that is a simple datatype/method holder
+    interface IRefillable
+    {
+        void Refill(int amt);
+
+        float FuelPercentage
+        {
+            get;
+            set;
+        }
+    }
+
+    public class Cat 
+    {
+        public int hunger;
+        public int maxHunger;
+        //Review his in book, pages-342-353
+        
+    }
+
+
+    
+
+}
